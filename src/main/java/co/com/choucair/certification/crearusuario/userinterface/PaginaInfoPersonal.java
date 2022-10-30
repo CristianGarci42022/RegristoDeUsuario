14ff32f4fba788dd212d8a334ac5ca2cca2b8fe1
CristianGarci42022 <cristiangarci42o22@gmail.com>
2022-10-29 20:57:37 -0500

    Complemento de mapeo de formulario
    Se realizo el mapeo de los campos para el formulario de la primera vista de datos personales del usuario

--- src/main/java/co/com/choucair/certification/crearusuario/userinterface/PaginaInfoPersonal.java
+++ src/main/java/co/com/choucair/certification/crearusuario/userinterface/PaginaInfoPersonal.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.By;
 public class PaginaInfoPersonal {
 
     public static final Target NOMBRE= Target.the("Campo para nombre de usuario")
-            .located(By.id(""));
+            .located(By.id("firstName"));
     public static final Target APELLIDO= Target.the("Campo para apellido de usuario")
-            .located(By.id(""));
+            .located(By.id("lastName"));
     public static final Target EMAIL= Target.the("Campo para email de usuario")
-            .located(By.id(""));
+            .located(By.id("email"));
     public static final Target MES_NACIMIENTO= Target.the("Campo para mes de nacimiento de usuario")
-            .located(By.id(""));
+            .located(By.id("birthMonth"));
     public static final Target DIA_NACIMIENTO= Target.the("Campo para dia de nacimiento de usuario")
-            .located(By.id(""));
+            .located(By.id("birthDay"));
     public static final Target ANO_NACIMIENTO= Target.the("Campo para año de nacimiento de usuario")
-            .located(By.id(""));
+            .located(By.id("birthYear"));
     public static final Target BOTON_DIRECION= Target.the("Boton para ir a seccion de direccion")
-            .located(By.id(""));
+            .located(By.xpath("//a[@class='btn btn-blue']"));
 }
--- src/test/java/co/com/choucair/certification/crearusuario/tasks/LLenarPaginaInfoPersona.java
+++ src/test/java/co/com/choucair/certification/crearusuario/tasks/LLenarPaginaInfoPersona.java
@@ -5,8 +5,13 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.SelectFromOptions;
 
 import java.util.List;
+
+import static co.com.choucair.certification.crearusuario.userinterface.PaginaInfoPersonal.*;
 
 public class LLenarPaginaInfoPersona implements Task {
 
@@ -22,6 +27,13 @@ public class LLenarPaginaInfoPersona implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-
+        actor.attemptsTo(Enter.theValue(datos.get(0).getStrNombre()).into(NOMBRE),
+                Enter.theValue(datos.get(0).getStrApellido()).into(APELLIDO),
+                Enter.theValue(datos.get(0).getStrEmail()).into(EMAIL),
+                SelectFromOptions.byVisibleText(datos.get(0).getStrMesNacimiento()).from(MES_NACIMIENTO),
+                SelectFromOptions.byVisibleText(datos.get(0).getStrDiaNacimiento()).from(DIA_NACIMIENTO),
+                SelectFromOptions.byVisibleText(datos.get(0).getStrAnoNacimiento()).from(ANO_NACIMIENTO),
+                Click.on(BOTON_DIRECION)
+            );
     }
 }
